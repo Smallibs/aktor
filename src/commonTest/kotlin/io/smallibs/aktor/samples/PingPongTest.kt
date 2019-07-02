fe384dbcacf3ca5a710bb6326ae1aafd2c8dbfd5
d-plaindoux <d.plaindoux@free.fr>
2019-07-02 06:55:14 +0200

    Review test and upgrade gradle to 5.5

--- src/commonTest/kotlin/io/smallibs/aktor/samples/PingPongTest.kt
+++ src/commonTest/kotlin/io/smallibs/aktor/samples/PingPongTest.kt
@@ -4,7 +4,7 @@ import io.smallibs.aktor.ActorReference
 import io.smallibs.aktor.Aktor
 import io.smallibs.aktor.ProtocolBehavior
 import io.smallibs.utils.Await
-import kotlinx.atomicfu.AtomicInt
+import kotlinx.atomicfu.AtomicBoolean
 import kotlinx.atomicfu.atomic
 import kotlin.test.Test
 
@@ -12,9 +12,9 @@ class PingPongTest {
 
     class PingPong(val sender: ActorReference<PingPong>)
 
-    private fun arbiter(nbEnded: AtomicInt): ProtocolBehavior<String> = { actor, message ->
+    private fun arbiter(endedGame: AtomicBoolean): ProtocolBehavior<String> = { actor, message ->
         println("${message.content} ending game ...")
-        nbEnded.incrementAndGet()
+        endedGame.getAndSet(true)
         actor.same()
     }
 
@@ -35,15 +35,15 @@ class PingPongTest {
     fun shouldPlayGame() {
         val aktor = Aktor.new("test")
 
-        val endedPlayers = atomic(0)
+        val endedGame = atomic(false)
 
-        val arbiter = aktor.actorFor(arbiter(endedPlayers))
+        val arbiter = aktor.actorFor(arbiter(endedGame))
         val ping = aktor.actorFor(player(arbiter, "ping"))
         val pong = aktor.actorFor(player(arbiter, "pong"))
 
         ping tell PingPong(pong)
 
-        Await(5000).until { endedPlayers.value == 1 }
+        Await(5000).until { endedGame.value }
 
         aktor.halt()
     }

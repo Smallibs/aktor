0fa0584a9c23c19733ca34aed303310ddd310caf
d-plaindoux <d.plaindoux@free.fr>
2020-08-13 08:51:33 +0200

    Upgrade versions in gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,45 +27,40 @@ repositories {
 
 kotlin {
     jvm()
-    js {
-    }
-    // For ARM, should be changed to iosArm32 or iosArm64
-    // For Linux, should be changed to e.g. linuxX64
-    // For MacOS, should be changed to e.g. macosX64
-    // For Windows, should be changed to e.g. mingwX64
-    // macosX64("macos")
+    js().nodejs()
+
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.0.1")
-                implementation("org.jetbrains.kotlinx:atomicfu-common:0.11.12")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.8")
+                implementation("org.jetbrains.kotlinx:atomicfu-common:0.14.2")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
-                implementation("org.awaitility:awaitility-kotlin:3.1.3")
             }
         }
         val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:0.27.0-eap13")
-                implementation("org.jetbrains.kotlinx:atomicfu:0.11.12")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.8")
+                implementation("org.jetbrains.kotlinx:atomicfu:0.14.2")
             }
         }
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
+                implementation("org.awaitility:awaitility-kotlin:3.1.3")
             }
         }
         val jsMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-js"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.0.1")
-                implementation("org.jetbrains.kotlinx:atomicfu-js:0.11.12")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.8")
+                implementation("org.jetbrains.kotlinx:atomicfu-js:0.14.2")
             }
         }
         val jsTest by getting {
